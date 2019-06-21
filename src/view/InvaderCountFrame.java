794b0846917fd1c90da231e1d7a0be729d8e6eec
halfershan33 <49019461+halfershan33@users.noreply.github.com>
2019-06-21 09:35:08 -0600

    update docString

--- src/view/InvaderCountFrame.java
+++ src/view/InvaderCountFrame.java
@@ -5,13 +5,21 @@ import model.GameInfoProvider;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * frame for holding the panel for Invader count
+ */
+
 public class InvaderCountFrame extends JFrame {
 
-
+    /**
+     * constructor for frame
+     * @param info provides methods to access information about current state of game
+     */
     public InvaderCountFrame(GameInfoProvider info){
 
         setTitle("Invader Count");
         setSize(200,200);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         add(new InvaderCountPanel(info));
         setVisible(true);
